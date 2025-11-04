175dd585334f552bce8d3dc076b64b21285397cd
Kaushik Mukherjee <kaushikmukherjee2709@gmail.com>
2025-11-04 22:42:39 +0530

    js route added

--- image_compressor_backend/dist/routes/imageRoutes.js
+++ image_compressor_backend/dist/routes/imageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { ImageControllers } from "../controllers/imageController";
+import { ImageControllers } from "../controllers/imageController.js";
 const Imagerouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 Imagerouter.post("/compress", upload.single("file"), ImageControllers);
