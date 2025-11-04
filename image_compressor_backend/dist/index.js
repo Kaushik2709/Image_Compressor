11fa3cfe586d83ee6a5ed4643aa0bae919ff696c
Kaushik Mukherjee <kaushikmukherjee2709@gmail.com>
2025-11-04 22:22:21 +0530

    deployment

--- image_compressor_backend/dist/index.js
+++ image_compressor_backend/dist/index.js
@@ -1,61 +1,45 @@
 import dotenv from "dotenv";
 import express from "express";
-import multer from "multer";
-import sharp from "sharp";
-import { createClient } from "@supabase/supabase-js";
-import fs from "fs";
 import path from "path";
-import { Imagerouter } from "./routes/imageRoutes.ts";
+import { Imagerouter } from "./routes/imageRoutes";
 import cors from "cors";
 import cluster from "cluster";
 import os from "os";
 import { fileURLToPath } from "url";
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 4000;
-
 // ✅ Enable CORS only for localhost:8080
-app.use(
-  cors({
-   origin:
-  process.env.NODE_ENV === "production"
-    ? "https://nologin-imagecompressor.onrender.com"
-    : "http://localhost:8080",
-
+app.use(cors({
+    origin: process.env.NODE_ENV === "production"
+        ? "https://nologin-imagecompressor.onrender.com"
+        : "http://localhost:8080",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
-  })
-);
-
+}));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const frontendPath = path.join(__dirname, "../image_compressor_frontend/dist");
 app.use(express.static(frontendPath));
-
 app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
 });
-
 // ✅ Parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/file", Imagerouter);
-
 const totalCPUs = os.cpus().length;
 if (cluster.isPrimary) {
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-  // console.log(`Primary ${process.pid} is running`);
-} else {
-  app.listen(PORT,()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
-    
-  })
+    for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+    // console.log(`Primary ${process.pid} is running`);
+}
+else {
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
 }
---
+++ image_compressor_backend/dist/routes/imageRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import multer from "multer";
+import { ImageControllers } from "../controllers/imageController";
+const Imagerouter = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+Imagerouter.post("/compress", upload.single("file"), ImageControllers);
+export { Imagerouter };
